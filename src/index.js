c062eded51f4a8d5366538d17f49003d816c9dce
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-06-17 13:09:06 -0500

    added webfontloader

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import App from "./components/App";
 import React from "react";
 import { render } from "react-dom";
 
+import WebFont from "webfontloader";
+
+WebFont.load({
+  google: {
+    families: ["Oswald:400,700", "Lato"]
+  }
+});
+
 render(<App />, document.getElementById("root"));
