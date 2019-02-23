1e9d4497026466740f6ba37c5c64231a6ce80d94
Jimmy Guzman <jimmy.guzman@outlook.com>
2019-02-23 16:52:24 -0600

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,54 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    publicPath: "/"
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'eslint-loader'
+          }
+        ]
       },
       {
         test: /\.html$/,
-        use: [{ loader: "html-loader", options: { minimize: true } }]
+        use: [{ loader: 'html-loader', options: { minimize: true } }]
       },
       {
         test: /\.(png|jpe?g)/i,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              name: "./img/[name].[ext]",
+              name: './img/[name].[ext]',
               limit: 10000
             }
           },
           {
-            loader: "img-loader"
+            loader: 'img-loader'
           }
         ]
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
         ]
       }
     ]
@@ -53,13 +58,13 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "src/index.html",
-      filename: "./index.html",
-      favicon: "src/img/favicon.ico"
+      template: 'src/index.html',
+      filename: './index.html',
+      favicon: 'src/img/favicon.ico'
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: '[name].css',
+      chunkFilename: '[id].css'
     })
   ]
-};
+}
