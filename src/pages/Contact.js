239e0ea9d5fb9c89ee2d80de771f669b7e80900b
Jimmy Guzman <jimmy.guzman@outlook.com>
2019-03-01 23:12:55 -0600

    use constants in Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,44 +1,38 @@
 import React from 'react'
 import { bool } from 'prop-types'
 
-import contact from '../data/contact.json'
+import { english, spanish } from '../data/contact.json'
 
 import { GithubSVG, EmailSVG, LinkedinSVG } from '../components/svgs'
 
 const EMAIL = 'hello@jimmyguzman.com'
+const LINKEDIN_URL = 'https://www.linkedin.com/in/jimmy-guzman-87bb2714a/'
+const GITHUB_URL = 'https://github.com/jimmy-guzman'
 
 const Contact = ({ isEnglish }) => (
   <main className='light'>
     <section className='contact grid__row'>
       <div className='card grid__col--6'>
-        <p>{isEnglish ? contact.english : contact.spanish}</p>
+        <p>{isEnglish ? english : spanish}</p>
         <ul>
           <li>
-            <a href='mailto:hello@jimmyguzman.com'>
+            <a href={`mailto:${EMAIL}`}>
               <EmailSVG width='50' height='50' />
             </a>
           </li>
           <li>
-            <a
-              href='https://www.linkedin.com/in/jimmy-guzman-87bb2714a/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
+            <a href={LINKEDIN_URL} target='_blank' rel='noopener noreferrer'>
               <LinkedinSVG width='50' height='50' />
             </a>
           </li>
           <li>
-            <a
-              href='https://github.com/jimmy-guzman'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
+            <a href={GITHUB_URL} target='_blank' rel='noopener noreferrer'>
               <GithubSVG width='50' height='50' />
             </a>
           </li>
         </ul>
         <p>
-          <a href='mailto:hello@jimmyguzman.com'>{EMAIL}</a>
+          <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
         </p>
       </div>
     </section>
